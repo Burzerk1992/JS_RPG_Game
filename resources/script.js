cd135b4a38d9ead02a4d8059e37a16de745d1b2e
Kevin Childers <kschilders92@gmail.com>
2025-08-09 15:46:27 -0400

    Updating before starting work on fighting monsters functions

--- resources/script.js
+++ resources/script.js
@@ -86,17 +86,36 @@ function buyHealth(){
 };
 
 function buyWeapon(){
-    if (gold >= 30){
-        gold -= 30;
-        currentWeaponIndex ++;
-        goldText.innerText = gold;
-        //uses the value of currentWeaponIndex and access the name key
-        let newWeapon = weapons[currentWeaponIndex].name;
-        text.innerText = "You now have a " + newWeapon + ".";
-        inventory.push(newWeapon);
-        text.innerText += " In your inventory you have: "
+    if (currentWeaponIndex < weapons.length-1) {
+      if (gold >= 30){
+          gold -= 30;
+          currentWeaponIndex++;
+          goldText.innerText = gold;
+          //uses the value of currentWeaponIndex and access the name key
+          let newWeapon = weapons[currentWeaponIndex].name;
+          text.innerText = "You now have a " + newWeapon + ".";
+          inventory.push(newWeapon);
+          text.innerText += " In your inventory you have: " + inventory;
+          } else {
+              text.innerText = "You do not have enough gold to buy a weapon."
+          }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold."
+        button2.onclick = sellWeapon;
     }
+};
 
+function sellWeapon(){
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!"
+    }
 };
 
 function fightSlime(){
@@ -109,14 +128,14 @@ function fightBeast(){
 
 function update(location){
     //sets the text of button1 and onclick action
-    button1.innerText = location["button text"][0];
-    button1.onclick = location["button functions"][0];
+    button1.innerText = location["button_text"][0];
+    button1.onclick = location["button_function"][0];
     //sets the text of button2 and onclick action
-    button2.innerText = location["button text"][1];
-    button2.onclick = location["button functions"][1];
+    button2.innerText = location["button_text"][1];
+    button2.onclick = location["button_function"][1];
     //sets the text of button3 and onclick action
-    button3.innerText = location["button text"][2];
-    button3.onclick =  location["button functions"][2];
+    button3.innerText = location["button_text"][2];
+    button3.onclick =  location["button_function"][2];
     //set text value to reflect the location of the player
     text.innerText = location.text;
 };
